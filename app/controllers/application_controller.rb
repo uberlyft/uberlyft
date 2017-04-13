c08c7c70b8f0933e83cc93d9e009727d44ff2a8d
DAVMCKIN <DAVIDJMCKIN@GMAIL.COM>
2017-04-13 17:02:57 -0400

    add time estimate for user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,13 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  # protect_from_forgery with: :exception
   before_action :current_user
 
 
   private
+
+  def require_user
+    render json: {error: "You need to be logged in to do this."} unless current_user
+  end
 
   def current_user
    @current_user ||= User.find(session[:user_id]) if session[:user_id]
