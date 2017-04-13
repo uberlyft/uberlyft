867dbc5686ac0eeeb64f660454dce91cda3b81ca
DAVMCKIN <DAVIDJMCKIN@GMAIL.COM>
2017-04-13 17:33:10 -0400

    adds controller actions to request rides, and get prices

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,18 +5,42 @@ class UsersController < ApplicationController
     client = Uber::Client.new do |config|
       config.server_token = (ENV['UBER_SERVER_TOKEN']).to_s
     end
-    location = Geocoder.search(request.remote_ip)
-    user_location = client.time_estimations(start_latitude: location.first.latitude, start_longitude: location.first.longitude)
-    render json: user_location
+    current_location = Geocoder.search(request.remote_ip)
+    user_locate = client.time_estimations(start_latitude: current_location.first.latitude, start_longitude: current_location.first.longitude)
+    render json: user_locate
   end
 
   def price_estimate
     client = Uber::Client.new do |config|
       config.server_token = (ENV['UBER_SERVER_TOKEN']).to_s
     end
-    location = Geocoder.search(request.remote_ip)
-    user_location = client.time_estimations(start_latitude: location.first.latitude, start_longitude: location.first.longitude),
-                             end_latitude: dlat, end_longitude: dlon)
+    current_location = Geocoder.search(request.remote_ip)
+    end_location = Geocoder.search(params[:address])
+    user_locate = client.time_estimations(start_latitude: current_location.first.latitude, start_longitude: current_location.first.longitude, end_latitude: end_location.first.latitude, end_longitude: end_location.first.latitude)
+    render json: user_locate
+  end
+
+  def history
+    client = Uber::Client.new do |config|
+      config.server_token = (ENV['UBER_SERVER_TOKEN']).to_s
+      config.client_id     = (ENV['UBER_CLIENT_ID']).to_s
+      config.client_secret = (ENV['UBER_CLIENT_SECRET']).to_s
+      config.bearer_token  = current_user.uber_token
+    end
+    client.history
+  end
+
+  def request_ride #simulated in sandbox, will not request actual ride
+    current_location = Geocoder.search(request.remote_ip)
+    end_location = Geocoder.search(params[:address])
+    client = Uber::Client.new do |config|
+      config.client_id     = (ENV['UBER_CLIENT_ID']).to_s
+      config.client_secret = (ENV['UBER_CLIENT_SECRET']).to_s
+      config.bearer_token  = current_user.uber_token
+    end
+    client.apply_surge 'product_id', 2.0
+
+    client.trip_request(product_id: product_id, start_latitude: current_location.first.latitude, start_longitude: current_location.first.longitude, end_latitude: end_location.first.latitude, end_longitude: end_location.first.latitude, surge_confirmation_id: surge_id)
   end
 
   # def current_user_latitude
