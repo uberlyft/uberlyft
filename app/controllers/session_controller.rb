d15292232f78a3c9a54e531fcebcdad302f97020
DAVMCKIN <DAVIDJMCKIN@GMAIL.COM>
2017-04-17 14:34:35 -0400

    cleanup and adding more to request a ride

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -2,28 +2,14 @@ class SessionController < ApplicationController
 
   def create
     auth = request.env['omniauth.auth']
-    Rails.logger.info request.env['omniauth.auth'].inspect
     user = User.sign_in_from_omniauth(auth, current_user)
     session[:user_id] = user.id
     if auth[:provider] == 'uber'
       redirect_to '/auth/lyft'
     elsif auth[:provider] == 'lyft'
       redirect_to '/users/time_estimate'
-    else #this will only trigger if auth is neither uber or lyft. what do you want to return in that scenario
+    else
+      render json: {error: "Could not display this provider's authentification", status: 400}, status: 400
     end
   end
-
-#   def associate_user
-#     @identity = Identity.find(params[:identity_id])
-#     if @identity.user
-#       render json: @identity.user, serializer: UserExpandedSerializer
-#     elsif current_user
-#       current_user.identities << @identity
-#       render json: current_user, serializer: UserExpandedSerializer
-#     else
-#       @user = User.create!(email: @identity.email || "", first_name: @identity.first_name || "", last_name: @identity.last_name || "")
-#       @user.identities << @identity
-#       render json: @user, serializer: UserExpandedSerializer
-#     end
-#   end
 end
