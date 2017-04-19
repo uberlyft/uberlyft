3cd969c30609a411e92a622bd37c14f59a52cc4c
YOUR NAMEJennifer <YOUR EMAIL ADDRESSjenniferbonner12@gmail.com>
2017-04-19 17:02:17 -0400

    feature corrections

--- app-react/src/Navbar.js
+++ app-react/src/Navbar.js
@@ -6,15 +6,15 @@ class Navbar extends Component {
 
     constructor(props) {
         super(props)
-        this.logout = this.logout.bind(this)
+        // this.logout = this.logout.bind(this)
     }
 
-logout() {
-    localStorage.clear();
-    location.href = '/?logout=yes';
-    localStorage.removeItem('token');
-    alert('you have signed out.');
-}
+  // logout() {
+  //     localStorage.clear();
+  //     location.href = '/?logout=yes';
+  //     localStorage.removeItem('token');
+  //     alert('you have signed out.');
+  // }
 
   render() {
     return <div>
@@ -27,11 +27,11 @@ logout() {
                         email: 'jdandturk@gmail.com'
                       }}
                     />
-                    <SideNavItem onClick={() => browserHistory.push('/')}><i className="fa fa-home fa-2x" aria-hidden="true"></i>HOME</SideNavItem>
+                    <SideNavItem onClick={() => browserHistory.push('/comparison')}><i className="fa fa-home fa-2x" aria-hidden="true"></i>HOME</SideNavItem>
                     <SideNavItem onClick={() => browserHistory.push('/how')}><i className="fa fa-wrench fa-2x" aria-hidden="true"></i>HOW IT WORKS</SideNavItem>
                     <SideNavItem onClick={() => browserHistory.push('/comparison')}><i className="fa fa-play fa-2x" aria-hidden="true"></i>RIDE REQUEST</SideNavItem>
                     <SideNavItem onClick={() => browserHistory.push('/history')}><i className="fa fa-history fa-2x" aria-hidden="true"></i>RIDE HISTORY</SideNavItem>
-                    <SideNavItem onClick={this.logout}><i className="fa fa-sign-in fa-2x" aria-hidden="true"></i>SIGN OUT</SideNavItem>
+                    {/*<SideNavItem onClick={this.logout}><i className="fa fa-sign-in fa-2x" aria-hidden="true"></i>SIGN OUT</SideNavItem>*/}
                   </SideNav>
       </div>
   }
