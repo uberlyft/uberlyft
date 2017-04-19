c328ae3b083d4a9ce3ee20d6063df2c62dca0bb6
YOUR NAMEJennifer <YOUR EMAIL ADDRESSjenniferbonner12@gmail.com>
2017-04-19 15:20:48 -0400

    Optimized video

--- app-react/src/Signinbutton.js
+++ app-react/src/Signinbutton.js
@@ -8,7 +8,7 @@ class Signinbutton extends Component {
             <div className="container">
               <div className="row">
                 <div className="col s4 push-s4 button-margin button-style">
-                    <a className="waves-effect waves-light btn black" onClick={() => location.href='https://538ab3ab.ngrok.io/auth/uber'}>SIGN IN</a>
+                    <a className="waves-effect waves-light btn black" onClick={() => location.href='/auth/uber'}>SIGN IN</a>
                 </div>
               </div>
             </div>
