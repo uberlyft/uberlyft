fb0e11f89711cc6c2b46f23b1428b979466dd96b
YOUR NAMEJennifer <YOUR EMAIL ADDRESSjenniferbonner12@gmail.com>
2017-04-27 12:11:37 -0400

    changed heroku link

--- app-react/src/index.js
+++ app-react/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import Routes from './Routes';
 import './index.css';
 
-window.apiHost = 'https://secure-harbor-46339.herokuapp.com';
+window.apiHost = 'hhttps://commuter-tiy.herokuapp.com/';
 
 ReactDOM.render(
   <Routes/>,
