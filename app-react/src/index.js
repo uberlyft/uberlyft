ba7525c5d0485dcc98356a297c293f287e7ecbe5
YOUR NAMEJennifer <YOUR EMAIL ADDRESSjenniferbonner12@gmail.com>
2017-04-27 12:21:40 -0400

    fixed new url

--- app-react/src/index.js
+++ app-react/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import Routes from './Routes';
 import './index.css';
 
-window.apiHost = 'hhttps://commuter-tiy.herokuapp.com/';
+window.apiHost = 'https://commuter-tiy.herokuapp.com/';
 
 ReactDOM.render(
   <Routes/>,
